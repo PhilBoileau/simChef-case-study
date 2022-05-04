31f5da7a68008c84ed876eb5172ebf188a30664e
Philippe Boileau <pboileau.swim@gmail.com>
2022-05-04 15:00:26 -0700

    defined linear dgp

---
+++ R/dgps/linear-dgp.R
@@ -0,0 +1,48 @@
+################################################################################
+# Data Generating Processes
+################################################################################
+
+# Input:
+#   n, an integer representing the number of observations to simulate
+#   cov_mat, a 100x100 covariance matrix of the covariates
+# Output:
+#   A tibble object containing 100 covariates, a binary treatment indicator,
+#   and the potential outcomes of n independently simulated observations in a
+#   perfect RCT.
+linear_dgp <- function(n, cov_mat = diag(1, nrow = 100)) {
+
+  # define the sparse coefficients vector for the main effects, and controls and
+  # treatment interactions
+  beta_main <- c(rep(2, 20), rep(0, 80))
+  beta_0 <- rep(0, 100)
+  beta_1 <- c(rep(5, 50), rep(0, 50))
+
+  # generate the biomarkers
+  W <- MASS::mvrnorm(n = n, mu = rep(0, 100), Sigma = cov_mat)
+  colnames(W) <- paste0("W", seq_len(100))
+
+  # simulate the binary treatment assignment
+  A <- ifelse(runif(n) < 0.5, 0, 1)
+
+  # simulate the outcomes under treatment and controls
+  epsilon_0 <- rnorm(n = n, mean = 0, sd = 0.5)
+  epsilon_1 <- rnorm(n = n, mean = 0, sd = 0.5)
+  W_t <- t(W)
+  main_effects <- crossprod(W_t, beta_main)
+  Y_0 <- as.vector(main_effects + crossprod(W_t, beta_0) + epsilon_0)
+  Y_1 <- as.vector(main_effects + crossprod(W_t, beta_1) + epsilon_1)
+
+  # define the observed outcome
+  Y <- ifelse(A == 0, Y_0, Y_1)
+
+  # assembled into a tibble
+  sample_tbl <- tibble::tibble(
+    "Y" = Y,
+    "Y_1" = Y_1,
+    "Y_0" = Y_0,
+    "A" = A
+  )
+  sample_tbl <- dplyr::bind_cols(sample_tbl, W)
+
+  return(sim_data)
+}
