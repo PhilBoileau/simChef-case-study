5c4fc33c5ba9174f35532fdca8a08290cc64d046
Philippe Boileau <pboileau.swim@gmail.com>
2022-05-05 11:00:46 -0700

    uniCATE method now accepts SL as outcome learning option

--- R/method-functions/uniCATE.R
+++ R/method-functions/uniCATE.R
@@ -2,7 +2,7 @@
 # uniCATE Function
 ################################################################################
 
-unicate_fun <- function(Y, A, W) {
+unicate_fun <- function(Y, A, W, use_sl = FALSE) {
 
   # assemble the data into a tibble
   sample_tbl <- tibble::tibble(Y = Y, A = A)
@@ -14,6 +14,40 @@ unicate_fun <- function(Y, A, W) {
   # define the propensity scores for a perfect RCT
   propensity_score_ls <- list("1" = 0.5, "0" = 0.5)
 
+  # set up the super learner, if necessary
+  if (use_sl) {
+
+    # define the interactions
+    interactions <- lapply(biomarker_names, function(b) c(b, "A"))
+    lrnr_interactions <- sl3::Lrnr_define_interactions$new(interactions)
+
+    # define the base learners
+    lrnr_lasso <- sl3::make_learner(
+      sl3::Pipeline, lrnr_interactions, sl3::Lrnr_glmnet$new(alpha = 1)
+    )
+    lrnr_enet <- sl3::make_learner(
+      sl3::Pipeline, lrnr_interactions, sl3::Lrnr_glmnet$new(alpha = 0,5)
+    )
+    lrnr_ridge <- sl3::make_learner(
+      sl3::Pipeline, lrnr_interactions, sl3::Lrnr_glmnet$new(alpha = 0)
+    )
+    lrnr_ranger <- sl3::make_learner(
+      sl3::Pipeline, lrnr_interactions, sl3::Lrnr_ranger$new()
+    )
+
+    # assemble the super learner
+    super_learner <- sl3::Lrnr_sl$new(
+      learners = sl3::make_learner(
+        sl3::Stack, lrnr_lasso, lrnr_enet, lrnr_ridge, lrnr_ranger,
+        sl3::Lrnr_mean$new()
+      ),
+      metalearner = sl3::make_learner(sl3::Lrnr_nnls)
+    )
+
+  } else {
+    super_learner <- NULL
+  }
+
   # wrap uniCATE
   unicate_tbl <- uniCATE::unicate(
     data = sample_tbl,
@@ -22,7 +56,8 @@ unicate_fun <- function(Y, A, W) {
     covariates = biomarker_names,
     biomarkers = biomarker_names,
     propensity_score_ls = propensity_score_ls,
-    v_folds = 10
+    v_folds = 5,
+    super_learner = super_learner
   )
 
   # extract the suspected treatment effect modifiers
