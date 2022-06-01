3e8b6a10b085c06af59b0f43ba110567473acf18
Philippe Boileau <pboileau.swim@gmail.com>
2022-06-01 10:37:39 -0400

    adding FDR reference line to summary plot

--- R/visualizer-functions/classification-summary-plot.R
+++ R/visualizer-functions/classification-summary-plot.R
@@ -8,19 +8,22 @@ summary_plot_fun <- function(eval_results) {
   emp_fdr_tbl <- eval_results$`Empirical FDR` %>%
     dplyr::mutate(
       metric = "FDR",
-      value = fdr
+      value = fdr,
+      yintercept = 0.05
     ) %>%
     dplyr::select(-fdr)
   emp_tpr_tbl <- eval_results$`Empirical TPR` %>%
     dplyr::mutate(
       metric = "TPR",
-      value = tpr
+      value = tpr,
+      yintercept = NA
     ) %>%
     dplyr::select(-tpr)
   emp_tnr_tbl <- eval_results$`Empirical TNR` %>%
     dplyr::mutate(
       metric = "TNR",
-      value = tnr
+      value = tnr,
+      yintercept = NA
     ) %>%
     dplyr::select(-tnr)
   comb_tbl <- bind_rows(emp_fdr_tbl, emp_tpr_tbl, emp_tnr_tbl)
@@ -29,6 +32,8 @@ summary_plot_fun <- function(eval_results) {
     ggplot2::aes(x = n, y = value, colour = as.factor(.method_name)) +
     ggplot2::geom_point(alpha = 0.7) +
     ggplot2::geom_line(alpha = 0.7) +
+    ggplot2::geom_hline(aes(yintercept = yintercept), colour = "red",
+                        linetype = 2, alpha = 0.5) +
     ggplot2::facet_grid(cols = ggplot2::vars(.dgp_name),
                         rows = ggplot2::vars(metric)) +
     ggplot2::xlab("Sample Size") +
