a4b79449f2c7e02923d90a2d23721a2ca952821d
Philippe Boileau <pboileau.swim@gmail.com>
2022-06-16 18:09:35 -0400

    omitting superlearner method from meal

--- R/meal.R
+++ R/meal.R
@@ -29,8 +29,7 @@ nonlinear_dgp <- create_dgp(.dgp_fun = nlm_tem_fun)
 source(here("R/method-functions/uniCATE.R"))
 source(here("R/method-functions/modified-covariates.R"))
 source(here("R/method-functions/augmented-modified-covariates.R"))
-unicate_lasso_method <- create_method(.method_fun = unicate_fun, use_sl = FALSE)
-unicate_sl_method <- create_method(.method_fun = unicate_fun, use_sl = TRUE)
+unicate_lasso_method <- create_method(.method_fun = unicate_fun)
 mod_cov_method <- create_method(.method_fun = mod_cov_fun)
 aug_mod_cov_method <- create_method(.method_fun = aug_mod_cov_fun)
 
@@ -51,12 +50,10 @@ experiment <- create_experiment(name = "empirical-fdr-comparison") %>%
   add_dgp(linear_dgp, name = "LM with TEM") %>%
   add_dgp(kinked_dgp, name = "Kinked with TEM") %>%
   add_dgp(nonlinear_dgp, name = "NLM with TEM") %>%
-  # is there a way to vary across all DGPs at the same time
   add_vary_across(.dgp = "LM with TEM", n = c(125, 250, 500)) %>%
   add_vary_across(.dgp = "Kinked with TEM", n = c(125, 250, 500)) %>%
   add_vary_across(.dgp = "NLM with TEM", n = c(125, 250, 500)) %>%
   add_method(unicate_lasso_method, name = "uniCATE (LASSO)") %>%
-#   add_method(unicate_sl_method, name = "uniCATE (SL)") %>%
   add_method(mod_cov_method, name = "Modified Covariates") %>%
   add_method(aug_mod_cov_method, name = "Augmented Modified Covariates") %>%
   add_evaluator(fdr_eval, name = "Empirical FDR") %>%
